cb02771b4cef2d6ec8b029a195a72e0ec0b3cec3
Produclem <clemguer09@gmail.com>
2024-03-21 20:20:20 +0100

    /who et readme

--- function/functions.js
+++ function/functions.js
@@ -1,21 +1,26 @@
-
-const mongoose = require("mongoose");
-
-
+const {get} = require("axios");
+const {headers} = require("../config/configOptionsJson");
 
 
+async function fetchPlayerNames() {
+    try {
+        const response = await get('https://servers-frontend.fivem.net/api/servers/single/eazypm', {headers});
 
+        const players = response.data.Data.players;
+        const clients = response.data.Data.clients;
+        const namesAndIds = players.map(player => {
+            const idIG = player.id;
+            const identifiers = player.identifiers;
+            const discordIdentifier = identifiers.find(id => id.startsWith('discord:'));
+            const name = player.name;
+            const id = discordIdentifier ? discordIdentifier.split(':')[1] : null;
+            return { name, id, idIG, identifiers };
+        });
 
-
-
-
-
-
-
-
-
-    module.exports = {
-
-
-
+        return namesAndIds;
+    } catch (error) {
+        console.error(error);
     }
+}
+
+module.exports = { fetchPlayerNames };
